8041cbee5ff5ec6c694b75fd592d9ebda3c6d688
ethan.alexander <ethan.alexander@digioutsource.com>
2022-10-28 10:30:33 +0200

    Changing text for Monday

--- sms-gateway/controllers/send_sms.js
+++ sms-gateway/controllers/send_sms.js
@@ -40,7 +40,7 @@ const readFile = (filename) => {
                 messageObject = {
                     from: '14155238886',
                     to: `${entry[3]}`,
-                    body: `Dear Valued Client, thank you for booking your vehicle in at SMG Century City. Your vehicle is booked for tomorrow at ${entry[10]} with ${entry[11]}. To adhere to the current social distancing measures, we request that you please remain in your vehicle upon arrival until one of our SMG representatives assists you. Please ensure all valuables have been removed from your vehicle as well as all discarded masks and tissues prior to check-in and kindly note we are a cashless site. Our Shuttle Service is operational should you not be able to make arrangements for your own transportation. We look forward to welcoming you to the dealership. Stay Safe, Stay Healthy, Stay Sanitized`
+                    body: `Dear Valued Client, thank you for booking your vehicle in at SMG Century City. Your vehicle is booked for Monday at ${entry[10]} with ${entry[11]}. To adhere to the current social distancing measures, we request that you please remain in your vehicle upon arrival until one of our SMG representatives assists you. Please ensure all valuables have been removed from your vehicle as well as all discarded masks and tissues prior to check-in and kindly note we are a cashless site. Our Shuttle Service is operational should you not be able to make arrangements for your own transportation. We look forward to welcoming you to the dealership. Stay Safe, Stay Healthy, Stay Sanitized`
                 };
                 sendMessage(messageObject);
                 // sendScheduledMessage(messageObject);
